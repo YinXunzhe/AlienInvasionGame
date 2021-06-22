d6326f707d4d5f15dfc202c774edfde3de3814a6
YinXunzhe <1728591569@qq.com>
2021-06-22 21:52:36 +0800

    创建管理游戏资源和行为的类 包括创建特定背景色的窗口和响应鼠标键盘输入

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
-
 import pygame
+
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -9,11 +11,11 @@ class AlienInvasion:
         """初始化游戏并创建游戏资源"""
         pygame.init()
 
-        self.screen=pygame.display.set_mode((1200,800))
+        self.settings=Settings()
+        self.screen=pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        #设置背景色
-        self.bg_color=(230,230,230)
+        self.ship=Ship(self)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -23,7 +25,8 @@ class AlienInvasion:
                 if event.type==pygame.QUIT:
                     sys.exit()
             #每次循环时都重绘屏幕
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             #让最近绘制的屏幕可见
             pygame.display.flip()
 
