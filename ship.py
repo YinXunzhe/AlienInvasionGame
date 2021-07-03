66c0e8db90d3b4eec67e70a7fdd6e545e59e0f7a
YinXunzhe <1728591569@qq.com>
2021-07-03 18:31:59 +0800

    重构key_events; q快捷键退出

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         """初始化并设置其初始位置"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings=ai_game.settings
 
         #加载飞船图像并获取其外接矩形
         self.image=pygame.image.load('images/ship.bmp')
@@ -14,6 +15,8 @@ class Ship:
 
         #新飞船放置在屏幕底部中央
         self.rect.midbottom=self.screen_rect.midbottom
+        # rect的x无法存储小数，需要定义新属性
+        self.x=float(self.rect.x)
 
         #移动标志
         self.moving_right=False;
@@ -25,7 +28,8 @@ class Ship:
 
     def update(self):
         """更新飞船位置"""
-        if self.moving_right:
-            self.rect.x+=1
-        if self.moving_left:
-            self.rect.x-=1
+        if self.moving_right and self.rect.right<self.screen_rect.right:
+            self.x+=self.settings.ship_speed
+        if self.moving_left and self.rect.left>self.screen_rect.left:
+            self.x-=self.settings.ship_speed
+        self.rect.x=self.x
