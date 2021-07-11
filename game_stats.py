dacf9c3c10b9c8a1cd6f549edf4e5e14043d3a3a
YinXunzhe <1728591569@qq.com>
2021-07-11 17:27:25 +0800

    Revert "增加计分、最高得分、等级和难度升级"
    This reverts commit ef826ca9

--- game_stats.py
+++ game_stats.py
@@ -5,11 +5,7 @@ class GameStats:
         self.reset_stats()
         # 游戏开始处于非活动状态
         self.game_active=False
-        # 最高得分
-        self.high_score = 0
 
     def reset_stats(self):
         """初始化游戏运行过程中可能变化的统计信息"""
-        self.ship_left=self.settings.ship_limit-1
-        self.score=0
-        self.level=1
+        self.ship_left=self.settings.ship_limit
