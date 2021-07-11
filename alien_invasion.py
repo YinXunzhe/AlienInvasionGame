baabfe82e9973a7a5cb359400f956649ef100a25
YinXunzhe <1728591569@qq.com>
2021-07-11 11:46:19 +0800

    添加Play按钮控制游戏开始 优化鼠标光标在游戏中隐藏

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
+from button import Button
 
 
 class AlienInvasion:
@@ -21,8 +22,9 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+        self.play_button = Button(self, "Play")
 
-        self.stats=GameStats(self)
+        self.stats = GameStats(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -51,6 +53,9 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
 
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -67,6 +72,26 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def _check_play_button(self, mouse_pos):
+        """玩家单击Play按钮时开始新游戏"""
+        button_checked = self.play_button.rect.collidepoint(mouse_pos)
+        # 仅在按下Play按钮并且游戏处于非活动状态时才重新开始游戏，防止游戏时误触
+        if button_checked and not self.stats.game_active:
+            # 重置游戏统计信息
+            self.stats.reset_stats()
+            self.stats.game_active = True
+
+            # 清空余下的外星人和子弹
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # 创建新的外星人并让飞船居中
+            self._creat_fleet()
+            self.ship.center_ship()
+
+            # 隐藏鼠标光标
+            pygame.mouse.set_visible(False)
 
     def _update_bullets(self):
         self.bullets.update()
@@ -130,9 +155,9 @@ class AlienInvasion:
 
     def _check_aliens_bottom(self):
         """检查是否有外星人触底"""
-        screen_rect=self.screen.get_rect()
+        screen_rect = self.screen.get_rect()
         for alien in self.aliens.sprites():
-            if alien.rect.bottom>=screen_rect.bottom:
+            if alien.rect.bottom >= screen_rect.bottom:
                 self._ship_hit()
                 break
 
@@ -149,7 +174,7 @@ class AlienInvasion:
         self.aliens.update()
 
         # 检查外星人和飞船的碰撞
-        if pygame.sprite.spritecollideany(self.ship,self.aliens):
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
             print("Ship hit!!!")
             self._ship_hit()
         # 检查外星人是否触底
@@ -157,22 +182,23 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """响应飞船被外星人撞到"""
-        if self.stats.ship_left>0:
+        if self.stats.ship_left > 0:
             # 拥有的飞船数量减一
-            self.stats.ship_left-=1
+            self.stats.ship_left -= 1
 
-            #清空余下的子弹和外星人
+            # 清空余下的子弹和外星人
             self.aliens.empty()
             self.bullets.empty()
 
             # 创建新的外星人，让飞船恢复到底部中央
             self._creat_fleet()
-            self.ship.ship_center_ship()
+            self.ship.center_ship()
 
             # 暂停
             sleep(0.5)
         else:
-            self.stats.game_active=False
+            self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _update_screen(self):
         # 每次循环时都重绘屏幕
@@ -181,6 +207,11 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # 如果非活动状态就绘制Play按钮
+        if not self.stats.game_active:
+            self.play_button.draw_button()
+
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
--- game_stats.py
+++ game_stats.py
@@ -3,7 +3,8 @@ class GameStats:
     def __init__(self,ai_game):
         self.settings=ai_game.settings
         self.reset_stats()
-        self.game_active=True
+        # 游戏开始处于非活动状态
+        self.game_active=False
 
     def reset_stats(self):
         """初始化游戏运行过程中可能变化的统计信息"""
